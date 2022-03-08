bfa82d3145360fd4e57b30fcb07790a3974724e7
lutzer <lutz@lu-re.de>
2022-03-08 14:41:21 +0100

    changed config

--- node/config.default.js
+++ node/config.default.js
@@ -12,14 +12,14 @@ var Config = {
 	fileDir: __dirname + '/files/',
 	maxFileSize: 1024*1024*200, //allowed file size for attached files
 	allowedImageFileTypes: ['image/jpeg','image/jpg','image/png','image/gif'],
-	allowedAttachmentFileTypes: ['audio/wav','audio/x-wav','audio/mpeg3','audio/x-mpeg3','audio/wave','image/jpeg','image/jpg','application/pdf'],
+	allowedAttachmentFileTypes: ['audio/wav','audio/x-wav','audio/x-m4a','audio/mpeg','audio/mpeg3','audio/x-mpeg3','audio/wave','audio/mp4','audio/aac','audio/ogg','audio/webm','audio/flac','video/webm','video/ogg','video/mp4','image/jpeg','image/jpg','image/png','application/pdf'],
 
 	/* SERVER CONFIG */
 	baseUrl : '/', // with trailing /
 	publicDir : '../www/',
 	adminDir : '../admin/',
 	hostname : false, // 127.0.0.1 = private, false = public
-	port : '8081',
+	port : '9091',
 
 	/* AUTH DATA */
 	authName: 'admin',
